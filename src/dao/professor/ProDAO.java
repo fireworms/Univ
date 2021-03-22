d92b15fe2e896aa72e83732a0bdee915a342be53
fireworms <fireworms0@gamil.com>
2021-03-22 15:18:49 +0900

    폴더 분리

--- src/dao/professor/ProDAO.java
+++ src/dao/professor/ProDAO.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-import DBest.DBConn;
+import db.DBConn;
 
 public class ProDAO {
 
--- src/db/DBConn.java
+++ src/db/DBConn.java
@@ -1,4 +1,4 @@
-package DBest;
+package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
